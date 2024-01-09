083167cd76c5d70465d5ed34e1c99288de33f8b3
quarkusio <no-reply@quarkus.io>
2024-01-09 14:07:26 +0000

    Initial commit

---
+++ src/native-test/java/com/fanduel/ks/agent/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.fanduel.ks.agent;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
